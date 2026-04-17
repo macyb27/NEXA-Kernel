1fc939b0d17e06a1cb888b6bd60df7b8ab8d0a02
macyb27 <mrehberg@gmx.de>
2026-04-17 07:11:30 +0200

    Refactor freeze.py to use KernelFreeze class

--- kernel_v20/core/freeze.py
+++ kernel_v20/core/freeze.py
@@ -1,13 +1,19 @@
-VERSION = "v21.0"
+class KernelFreeze:
+    """
+    Final architecture freeze marker.
+    No structural changes allowed beyond this point.
+    """
 
-@staticmethod
-def assert_frozen():
-    return True
+    VERSION = "v21.0"
 
-@staticmethod
-def info():
-    return {
-        "status": "frozen",
-        "version": KernelFreeze.VERSION,
-        "modification_policy": "NO_CORE_CHANGES_ALLOWED"
-    }
+    @staticmethod
+    def assert_frozen():
+        return True
+
+    @staticmethod
+    def info():
+        return {
+            "status": "frozen",
+            "version": KernelFreeze.VERSION,
+            "modification_policy": "NO_CORE_CHANGES_ALLOWED"
+        }
